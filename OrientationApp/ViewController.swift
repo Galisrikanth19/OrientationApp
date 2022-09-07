78ac20229224979ff12fe860bcdf570e7a381649
Srikanth <Srikanth@webappclouds.com>
2022-09-07 14:54:05 +0530

    Initial Commit

---
+++ OrientationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrientationApp
+//
+//  Created by Webappclouds on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
